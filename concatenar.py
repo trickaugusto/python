f04e21a42acce202701620b5e4b15b49b4d22f94
Patrick <trickaugusto1206@gmail.com>
2021-04-15 23:07:10 -0300

    exercicios

--- concatenar.py
+++ concatenar.py
@@ -1,1 +1,5 @@
-print("Patrick " + "Augusto")
+primeiro_nome = "Patrick"
+segundo_nome = "Augusto"
+idade = input("Digite sua idade: ")
+
+print(primeiro_nome + " " + segundo_nome + " " + idade + " anos")
--- operacoes-numericas.py
+++ operacoes-numericas.py
@@ -1,8 +1,27 @@
-a = 3
-b = 5
+""" 
 
 print(a + b)
 print(a - b)
 print(a / b)
 print(a * b)
-print(a % b)
+print(a % b) 
+
+"""
+
+a = int(input("Digite o primeiro A: "))
+b = int(input("Digite o segundo B: "))
+
+resultado_soma = a + b
+print("A + B = " + str(resultado_soma))
+
+resultado_subtracao = a - b
+print("A - B = " + str(resultado_subtracao))
+
+resultado_multiplicacao = a * b
+print("A * B = " + str(resultado_multiplicacao))
+
+resultado_divisao = a / b
+print("A / B = " + str(resultado_divisao))
+
+resultado_resto = a % b
+print("A % B = " + str(resultado_resto))
